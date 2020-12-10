c62691158f3aa2ea022d1d727ec48429b4b5058a
Nadantikaa <74657101+Nadantikaa@users.noreply.github.com>
2020-12-10 14:44:43 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,38 @@
+class Ball {
+    constructor(x,y,radius){
+        var options = {
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2,
+        }
+        this.body = Bodies.circle(x,y,radius)
+        this.radius = radius
+        
+        World.add(world,this.body);
+  
+    }
+  
+  
+  
+  display() {
+    var pos = this.body.position
+    push();
+    translate(pos.y,pos.x);
+    fill("white")
+  
+    ellipseMode(CENTER);
+    ellipse(0,0,this.radius,this.radius);
+    pop();
+  
+  }
+  }
+  
+  function keyPressed(){
+  if(keyCode === UP_ARROW){
+  
+    Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+  }
+  
+  }
+  
